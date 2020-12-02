82c6264653ba485f9ea030ce8aeeef0c964b3ac6
Achmad Muchlasin <47469965+muchlaseen@users.noreply.github.com>
2020-12-02 13:23:01 +0700

    Rename variable each panels

--- src/student/management/system/StudentFrame.java
+++ src/student/management/system/StudentFrame.java
@@ -27,38 +27,38 @@ public class StudentFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jPanel4 = new javax.swing.JPanel();
+        mainPanel = new javax.swing.JPanel();
+        titlePanel = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jPanel3 = new javax.swing.JPanel();
+        separatorPanel = new javax.swing.JPanel();
         jSeparator1 = new javax.swing.JSeparator();
-        jPanel5 = new javax.swing.JPanel();
-        jPanel9 = new javax.swing.JPanel();
-        jPanel6 = new javax.swing.JPanel();
+        formPanel = new javax.swing.JPanel();
+        nameInputPanel = new javax.swing.JPanel();
+        firstNamePanel = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         jTextField6 = new javax.swing.JTextField();
-        jPanel7 = new javax.swing.JPanel();
+        lastNamePanel = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
         jTextField7 = new javax.swing.JTextField();
-        jPanel10 = new javax.swing.JPanel();
-        jPanel8 = new javax.swing.JPanel();
+        otherInputPanel = new javax.swing.JPanel();
+        npmPanel = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
         jTextField5 = new javax.swing.JTextField();
-        jPanel11 = new javax.swing.JPanel();
+        jurusanPanel = new javax.swing.JPanel();
         jLabel5 = new javax.swing.JLabel();
         jComboBox1 = new javax.swing.JComboBox<>();
-        jPanel12 = new javax.swing.JPanel();
+        alamatPanel = new javax.swing.JPanel();
         jLabel6 = new javax.swing.JLabel();
         jTextField4 = new javax.swing.JTextField();
-        jPanel15 = new javax.swing.JPanel();
+        phonePanel = new javax.swing.JPanel();
         jLabel7 = new javax.swing.JLabel();
         jTextField8 = new javax.swing.JTextField();
-        jPanel13 = new javax.swing.JPanel();
-        jPanel14 = new javax.swing.JPanel();
+        footerPanel = new javax.swing.JPanel();
+        actionPanel = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
-        jPanel2 = new javax.swing.JPanel();
+        tablePanel = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
 
@@ -68,127 +68,127 @@ public class StudentFrame extends javax.swing.JFrame {
         setMinimumSize(new java.awt.Dimension(800, 550));
         setPreferredSize(new java.awt.Dimension(1000, 550));
 
-        jPanel1.setPreferredSize(new java.awt.Dimension(400, 0));
+        mainPanel.setPreferredSize(new java.awt.Dimension(400, 0));
 
-        jPanel4.setPreferredSize(new java.awt.Dimension(400, 70));
+        titlePanel.setPreferredSize(new java.awt.Dimension(400, 70));
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 20)); // NOI18N
         jLabel1.setText("Student Management System");
 
-        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
-        jPanel4.setLayout(jPanel4Layout);
-        jPanel4Layout.setHorizontalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel4Layout.createSequentialGroup()
+        javax.swing.GroupLayout titlePanelLayout = new javax.swing.GroupLayout(titlePanel);
+        titlePanel.setLayout(titlePanelLayout);
+        titlePanelLayout.setHorizontalGroup(
+            titlePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(titlePanelLayout.createSequentialGroup()
                 .addGap(54, 54, 54)
                 .addComponent(jLabel1)
                 .addContainerGap(67, Short.MAX_VALUE))
         );
-        jPanel4Layout.setVerticalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel4Layout.createSequentialGroup()
+        titlePanelLayout.setVerticalGroup(
+            titlePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(titlePanelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(21, Short.MAX_VALUE))
         );
 
-        jPanel1.add(jPanel4);
+        mainPanel.add(titlePanel);
 
-        jPanel3.setPreferredSize(new java.awt.Dimension(400, 10));
+        separatorPanel.setPreferredSize(new java.awt.Dimension(400, 10));
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
+        javax.swing.GroupLayout separatorPanelLayout = new javax.swing.GroupLayout(separatorPanel);
+        separatorPanel.setLayout(separatorPanelLayout);
+        separatorPanelLayout.setHorizontalGroup(
+            separatorPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(separatorPanelLayout.createSequentialGroup()
                 .addGap(82, 82, 82)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 226, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(92, Short.MAX_VALUE))
         );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+        separatorPanelLayout.setVerticalGroup(
+            separatorPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, separatorPanelLayout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
-        jPanel1.add(jPanel3);
+        mainPanel.add(separatorPanel);
 
-        jPanel5.setPreferredSize(new java.awt.Dimension(400, 300));
+        formPanel.setPreferredSize(new java.awt.Dimension(400, 300));
 
-        jPanel9.setPreferredSize(new java.awt.Dimension(400, 60));
+        nameInputPanel.setPreferredSize(new java.awt.Dimension(400, 60));
 
-        jPanel6.setPreferredSize(new java.awt.Dimension(150, 50));
-        jPanel6.setLayout(new java.awt.BorderLayout(0, 4));
+        firstNamePanel.setPreferredSize(new java.awt.Dimension(150, 50));
+        firstNamePanel.setLayout(new java.awt.BorderLayout(0, 4));
 
         jLabel2.setText("Nama Depan");
-        jPanel6.add(jLabel2, java.awt.BorderLayout.PAGE_START);
-        jPanel6.add(jTextField6, java.awt.BorderLayout.CENTER);
+        firstNamePanel.add(jLabel2, java.awt.BorderLayout.PAGE_START);
+        firstNamePanel.add(jTextField6, java.awt.BorderLayout.CENTER);
 
-        jPanel9.add(jPanel6);
+        nameInputPanel.add(firstNamePanel);
 
-        jPanel7.setPreferredSize(new java.awt.Dimension(150, 50));
-        jPanel7.setLayout(new java.awt.BorderLayout(0, 4));
+        lastNamePanel.setPreferredSize(new java.awt.Dimension(150, 50));
+        lastNamePanel.setLayout(new java.awt.BorderLayout(0, 4));
 
         jLabel3.setText("Nama Belakang");
-        jPanel7.add(jLabel3, java.awt.BorderLayout.NORTH);
-        jPanel7.add(jTextField7, java.awt.BorderLayout.CENTER);
+        lastNamePanel.add(jLabel3, java.awt.BorderLayout.NORTH);
+        lastNamePanel.add(jTextField7, java.awt.BorderLayout.CENTER);
 
-        jPanel9.add(jPanel7);
+        nameInputPanel.add(lastNamePanel);
 
-        jPanel5.add(jPanel9);
+        formPanel.add(nameInputPanel);
 
-        jPanel10.setPreferredSize(new java.awt.Dimension(400, 240));
+        otherInputPanel.setPreferredSize(new java.awt.Dimension(400, 240));
 
-        jPanel8.setPreferredSize(new java.awt.Dimension(305, 50));
-        jPanel8.setLayout(new java.awt.BorderLayout(0, 4));
+        npmPanel.setPreferredSize(new java.awt.Dimension(305, 50));
+        npmPanel.setLayout(new java.awt.BorderLayout(0, 4));
 
         jLabel4.setText("NIM");
-        jPanel8.add(jLabel4, java.awt.BorderLayout.PAGE_START);
-        jPanel8.add(jTextField5, java.awt.BorderLayout.CENTER);
+        npmPanel.add(jLabel4, java.awt.BorderLayout.PAGE_START);
+        npmPanel.add(jTextField5, java.awt.BorderLayout.CENTER);
 
-        jPanel10.add(jPanel8);
+        otherInputPanel.add(npmPanel);
 
-        jPanel11.setPreferredSize(new java.awt.Dimension(305, 50));
-        jPanel11.setLayout(new java.awt.BorderLayout(0, 4));
+        jurusanPanel.setPreferredSize(new java.awt.Dimension(305, 50));
+        jurusanPanel.setLayout(new java.awt.BorderLayout(0, 4));
 
         jLabel5.setText("Jurusan");
-        jPanel11.add(jLabel5, java.awt.BorderLayout.PAGE_START);
+        jurusanPanel.add(jLabel5, java.awt.BorderLayout.PAGE_START);
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "--", "Teknik Informatika", "Teknik Sipil", "Teknik Industri", "Teknik Kelautan" }));
-        jPanel11.add(jComboBox1, java.awt.BorderLayout.CENTER);
+        jurusanPanel.add(jComboBox1, java.awt.BorderLayout.CENTER);
 
-        jPanel10.add(jPanel11);
+        otherInputPanel.add(jurusanPanel);
 
-        jPanel12.setPreferredSize(new java.awt.Dimension(305, 50));
-        jPanel12.setLayout(new java.awt.BorderLayout(0, 4));
+        alamatPanel.setPreferredSize(new java.awt.Dimension(305, 50));
+        alamatPanel.setLayout(new java.awt.BorderLayout(0, 4));
 
         jLabel6.setText("Alamat");
-        jPanel12.add(jLabel6, java.awt.BorderLayout.PAGE_START);
-        jPanel12.add(jTextField4, java.awt.BorderLayout.CENTER);
+        alamatPanel.add(jLabel6, java.awt.BorderLayout.PAGE_START);
+        alamatPanel.add(jTextField4, java.awt.BorderLayout.CENTER);
 
-        jPanel10.add(jPanel12);
+        otherInputPanel.add(alamatPanel);
 
-        jPanel15.setPreferredSize(new java.awt.Dimension(305, 50));
-        jPanel15.setLayout(new java.awt.BorderLayout(0, 4));
+        phonePanel.setPreferredSize(new java.awt.Dimension(305, 50));
+        phonePanel.setLayout(new java.awt.BorderLayout(0, 4));
 
         jLabel7.setText("No. Telpon");
-        jPanel15.add(jLabel7, java.awt.BorderLayout.PAGE_START);
-        jPanel15.add(jTextField8, java.awt.BorderLayout.CENTER);
+        phonePanel.add(jLabel7, java.awt.BorderLayout.PAGE_START);
+        phonePanel.add(jTextField8, java.awt.BorderLayout.CENTER);
 
-        jPanel10.add(jPanel15);
+        otherInputPanel.add(phonePanel);
 
-        jPanel5.add(jPanel10);
+        formPanel.add(otherInputPanel);
 
-        jPanel1.add(jPanel5);
+        mainPanel.add(formPanel);
 
-        jPanel13.setPreferredSize(new java.awt.Dimension(400, 100));
+        footerPanel.setPreferredSize(new java.awt.Dimension(400, 100));
 
-        jPanel14.setLayout(new java.awt.BorderLayout(0, 5));
+        actionPanel.setLayout(new java.awt.BorderLayout(0, 5));
 
         jButton1.setText("Tambah");
         jButton1.setPreferredSize(new java.awt.Dimension(150, 23));
-        jPanel14.add(jButton1, java.awt.BorderLayout.WEST);
+        actionPanel.add(jButton1, java.awt.BorderLayout.WEST);
 
         jButton2.setText("Update");
         jButton2.setPreferredSize(new java.awt.Dimension(150, 33));
@@ -197,36 +197,36 @@ public class StudentFrame extends javax.swing.JFrame {
                 jButton2ActionPerformed(evt);
             }
         });
-        jPanel14.add(jButton2, java.awt.BorderLayout.EAST);
+        actionPanel.add(jButton2, java.awt.BorderLayout.EAST);
 
         jButton4.setText("Delete");
         jButton4.setPreferredSize(new java.awt.Dimension(73, 32));
-        jPanel14.add(jButton4, java.awt.BorderLayout.SOUTH);
+        actionPanel.add(jButton4, java.awt.BorderLayout.SOUTH);
 
-        javax.swing.GroupLayout jPanel13Layout = new javax.swing.GroupLayout(jPanel13);
-        jPanel13.setLayout(jPanel13Layout);
-        jPanel13Layout.setHorizontalGroup(
-            jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel13Layout.createSequentialGroup()
+        javax.swing.GroupLayout footerPanelLayout = new javax.swing.GroupLayout(footerPanel);
+        footerPanel.setLayout(footerPanelLayout);
+        footerPanelLayout.setHorizontalGroup(
+            footerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(footerPanelLayout.createSequentialGroup()
                 .addGap(46, 46, 46)
-                .addComponent(jPanel14, javax.swing.GroupLayout.PREFERRED_SIZE, 306, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(actionPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 306, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(48, Short.MAX_VALUE))
         );
-        jPanel13Layout.setVerticalGroup(
-            jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel13Layout.createSequentialGroup()
+        footerPanelLayout.setVerticalGroup(
+            footerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(footerPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel14, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(actionPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(19, Short.MAX_VALUE))
         );
 
-        jPanel1.add(jPanel13);
+        mainPanel.add(footerPanel);
 
-        getContentPane().add(jPanel1, java.awt.BorderLayout.WEST);
+        getContentPane().add(mainPanel, java.awt.BorderLayout.WEST);
 
-        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel2.setPreferredSize(new java.awt.Dimension(600, 430));
-        jPanel2.setLayout(new java.awt.BorderLayout(0, 4));
+        tablePanel.setBackground(new java.awt.Color(255, 255, 255));
+        tablePanel.setPreferredSize(new java.awt.Dimension(600, 430));
+        tablePanel.setLayout(new java.awt.BorderLayout(0, 4));
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -249,9 +249,9 @@ public class StudentFrame extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(jTable1);
 
-        jPanel2.add(jScrollPane1, java.awt.BorderLayout.CENTER);
+        tablePanel.add(jScrollPane1, java.awt.BorderLayout.CENTER);
 
-        getContentPane().add(jPanel2, java.awt.BorderLayout.CENTER);
+        getContentPane().add(tablePanel, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -262,6 +262,11 @@ public class StudentFrame extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel actionPanel;
+    private javax.swing.JPanel alamatPanel;
+    private javax.swing.JPanel firstNamePanel;
+    private javax.swing.JPanel footerPanel;
+    private javax.swing.JPanel formPanel;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton4;
@@ -273,21 +278,6 @@ public class StudentFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel10;
-    private javax.swing.JPanel jPanel11;
-    private javax.swing.JPanel jPanel12;
-    private javax.swing.JPanel jPanel13;
-    private javax.swing.JPanel jPanel14;
-    private javax.swing.JPanel jPanel15;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JPanel jPanel4;
-    private javax.swing.JPanel jPanel5;
-    private javax.swing.JPanel jPanel6;
-    private javax.swing.JPanel jPanel7;
-    private javax.swing.JPanel jPanel8;
-    private javax.swing.JPanel jPanel9;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JTable jTable1;
@@ -296,5 +286,15 @@ public class StudentFrame extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField6;
     private javax.swing.JTextField jTextField7;
     private javax.swing.JTextField jTextField8;
+    private javax.swing.JPanel jurusanPanel;
+    private javax.swing.JPanel lastNamePanel;
+    private javax.swing.JPanel mainPanel;
+    private javax.swing.JPanel nameInputPanel;
+    private javax.swing.JPanel npmPanel;
+    private javax.swing.JPanel otherInputPanel;
+    private javax.swing.JPanel phonePanel;
+    private javax.swing.JPanel separatorPanel;
+    private javax.swing.JPanel tablePanel;
+    private javax.swing.JPanel titlePanel;
     // End of variables declaration//GEN-END:variables
 }
